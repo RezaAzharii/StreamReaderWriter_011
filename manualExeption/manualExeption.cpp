4e663a76e0c75643ecf56a36e04d53b62adc9559
RezaAzharii <rezaazhari867@gmail.com>
2023-06-21 15:57:31 +0700

    membuat catch jika yang throw lempar merupakan integer maka aka menampilkan "default pengecualian diieksekusi"

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -13,4 +13,9 @@ int main() {
 		cout << "Pengecualian Tidak Akan Di Eksekusi" << endl;
 	}
 
-}
+	catch (...) {
+		/*jika selain integer maka block ini akan dieksekusi*/
+		cout << "default Pengecualian dieksekusi" << endl;
+	}
+	return 0;
+ }
