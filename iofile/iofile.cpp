83e1e18ed083082d13f314921c09b9c603ab3c34
RezaAzharii <rezaazhari867@gmail.com>
2023-06-21 15:34:08 +0700

    membuat looping kondisi dimana jika memasukan karakter q maka program berhenti

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -12,4 +12,16 @@ int main() {
 	outfile.open("ContoFile.txt");
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "_";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	
 }
