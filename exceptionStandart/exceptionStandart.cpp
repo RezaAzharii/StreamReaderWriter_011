f14084e3b8a76d3c239761ee5c77e5f13147f9ac
RezaAzharii <rezaazhari867@gmail.com>
2023-06-21 16:03:11 +0700

    membuat try

---
+++ exceptionStandart/exceptionStandart.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan 
+#include <array>
+//untul obyek array yang akan kita gunakan
+using namespace std;
+
+int main() 
+{
+	cout << "Awal Program" << endl; //penanda 1....
+	try {
+		array<int, 3> data = { 3, 5, 7 };
+		//pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		//memanggil array elemen ke 5
+	}
+}
